913a63abcdb5e2d4b49d5ef97a81bd053bf4f36a
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-05-06 23:20:07 +0000

    Delete fixed

--- src/views/PatientPage.vue
+++ src/views/PatientPage.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="vh-100 mt-5">
-    <div class="container-fluid  h-custom mt-5">
-  <div class="container mt-5 flex gap-5 px-5">
-    <!-- <table class="table">
+    <div class="container-fluid h-custom mt-5">
+      <div class="container mt-5 flex gap-5 px-5">
+        <!-- <table class="table">
       <thead>
       <tr>
         <th>Subject</th>
@@ -19,83 +19,176 @@
 
       </tbody>
     </table> -->
-<!-- ---------- -->
-<div v-for="result in result" :key="result">
-  <div class="flex justify-center pb-3 " >
-  <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-    <div class="flex justify-center gap-4">
-    <button type="button" class=" edit inline-block px-6 py-2.5 mb-2 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><i class="fa-solid fa-pen-to-square"></i></button>
-    <button type="button" class=" edit inline-block px-6 py-2.5 mb-2 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"><i class="fa-solid fa-trash"></i></button>
+        <!-- ---------- -->
+        <div v-for="result in result" :key="result">
+          <div class="flex justify-center pb-3">
+            <div
+              class="block rounded-lg shadow-lg bg-white max-w-sm text-center"
+            >
+              <div class="flex justify-center gap-4">
+                <button
+                  type="button"
+                  class="
+                    edit
+                    inline-block
+                    px-6
+                    py-2.5
+                    mb-2
+                    bg-blue-600
+                    text-white
+                    font-medium
+                    text-xs
+                    leading-tight
+                    uppercase
+                    rounded-full
+                    shadow-md
+                    hover:bg-blue-700 hover:shadow-lg
+                    focus:bg-blue-700
+                    focus:shadow-lg
+                    focus:outline-none
+                    focus:ring-0
+                    active:bg-blue-800 active:shadow-lg
+                    transition
+                    duration-150
+                    ease-in-out
+                  "
+                  
+                >
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button
+                  type="button"
+                  class="
+                    edit
+                    inline-block
+                    px-6
+                    py-2.5
+                    mb-2
+                    bg-red-600
+                    text-white
+                    font-medium
+                    text-xs
+                    leading-tight
+                    uppercase
+                    rounded-full
+                    shadow-md
+                    hover:bg-red-700 hover:shadow-lg
+                    focus:bg-red-700
+                    focus:shadow-lg
+                    focus:outline-none
+                    focus:ring-0
+                    active:bg-red-700 active:shadow-lg
+                    transition
+                    duration-150
+                    ease-in-out
+                  "
+                  @click="deleteAppointment( result.IDR)"
+                >
+                  <i class="fa-solid fa-trash"></i>
+                </button>
+              </div>
+              <div class="py-3 px-6 border-b border-gray-300">
+                <p class="text-cyan-700 font-semibold">Date</p>
+                <p class="bg-cyan-600 rounded-xl py-2 text-white">
+                  {{ result.dateRDV }}
+                </p>
+              </div>
+              <div class="p-6">
+                <h5 class="text-cyan-700 font-semibold mb-2">Sujet</h5>
+                <p class="bg-cyan-600 rounded-xl py-2 px-3 text-white mb-4">
+                  {{ result.Sujet }}
+                </p>
+              </div>
+              <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
+                <p class="text-cyan-700 font-semibold mb-2">Heure</p>
+                <p class="bg-cyan-600 rounded-xl py-2 px-3 text-white mb-4">
+                  {{ result.begin }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- ---------------- -->
+      </div>
     </div>
-    <div class="py-3 px-6 border-b border-gray-300">
-      <p class="text-cyan-700 font-semibold">Date</p>
-      <p class="bg-cyan-600 rounded-xl py-2 text-white">{{result.dateRDV}}</p>
-    </div>
-    <div class="p-6">
-      <h5 class="text-cyan-700 font-semibold mb-2">Sujet</h5>
-      <p class="bg-cyan-600 rounded-xl py-2 px-3 text-white mb-4">
-       {{ result.Sujet }}
-      </p>
-    </div>
-    <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
-      <p class="text-cyan-700 font-semibold mb-2">Heure</p>
-     <p class="bg-cyan-600 rounded-xl py-2 px-3 text-white mb-4"> {{ result.begin }}</p>
-    </div>
-  </div>
-  </div>
-  </div>
-<!-- ---------------- -->
 
+    <!-- popup -->
 
-
-  </div>
-    </div>
+    <!-- --- -->
   </section>
 </template>
 
 <script>
+import swal from "sweetalert";
+
 export default {
   name: "PatientPage",
-  data(){
-    return{
-      sujet:'',
-      date:'',
-      heure:'',
-      result:[]
-    }
+  data() {
+    return {
+      sujet: "",
+      date: "",
+      heure: "",
+      result: [],
+    };
   },
 
-  methods:{
-    async getAppointment(){
-     const id =localStorage.getItem('id')
-      const data = await fetch('http://localhost/app/appointments/MyAppointments/'+id,
-          {method: 'POST',
-                headers: {'content-type':'application/json'},
-
-             })
-      if (data.status === 200){
-        let result = await  data.json();
-        console.log(result)
+  methods: {
+    async getAppointment() {
+      const id = localStorage.getItem("id");
+      const data = await fetch(
+        "http://localhost/app/appointments/MyAppointments/" + id,
+        { method: "POST", headers: { "content-type": "application/json" } }
+      );
+      if (data.status === 200) {
+        let result = await data.json();
+        // console.log(result);
         this.result = result;
-        this.sujet = result[0].Sujet;
-        this.date = result[0].dateRDV;
-        this.heure = result[0].begin;
-        console.log(this.sujet)
-
-
+        // this.sujet = result[0].Sujet;
+        // this.date = result[0].dateRDV;
+        // this.heure = result[0].begin;
+        // console.log(this.sujet);
       }
     },
 
-   
+   async deleteAppointment(RID){
+     
+     const result ={id : RID}
+     const data = await fetch(
+        "http://localhost/app/appointments/deleteAppointment",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(result),
+        }
+
+        
+      );
+
+      if (data.status === 200) {
+        console.log("Item Deleted");
+        swal({
+          title: "Good job!",
+          text: "You clicked the button!",
+          icon: "success",
+          button: "Aww yiss!",
+        });
+        this.$router.push("PatientPage");
+        this.getAppointment()
+      } else {
+        console.log("Failure");
+      }
+    },
   },
   beforeMount() {
-    this.getAppointment()
-  }
-}
+    this.getAppointment();
+  },
+};
 </script>
 
 <style scoped>
- table{
-   margin-top: 7em;
- }
+table {
+  margin-top: 7em;
+}
 </style>
--- src/views/bookAppointment.vue
+++ src/views/bookAppointment.vue
@@ -27,11 +27,11 @@
               <textarea required id="message" v-model="form.sujet" name="message" @input="getTextSubject($event.target.value)" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
           </div> -->
-          <!-- btn -->
-          <!-- <div class="flex justify-center mt--1 "> -->
+  <!-- btn -->
+  <!-- <div class="flex justify-center mt--1 "> -->
 
-       <!--     <button @click="getAppointment()" class="inline-flex font-medium  rounded-2xl text-neutral bg-primary-0 border-0 py-2 px-8 focus:outline-none  text-lg">Get an appointment</button> -->
-            <!-- <input type="submit" value="getAppointment" class="btn text-neutral btn-block bg-blue-500">
+  <!--     <button @click="getAppointment()" class="inline-flex font-medium  rounded-2xl text-neutral bg-primary-0 border-0 py-2 px-8 focus:outline-none  text-lg">Get an appointment</button> -->
+  <!-- <input type="submit" value="getAppointment" class="btn text-neutral btn-block bg-blue-500">
           </div>
             </form>
         </div>
@@ -41,164 +41,306 @@
   </section> -->
 
   <section class="text-gray-600 body-font relative">
-  <div class="absolute inset-0 bg-gray-300">
-    <iframe style="filter: grayscale(1) contrast(1.2) opacity(0.4);" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" width="100%" height="100%" frameborder="0"></iframe>
-  </div>
-  <div class="container px-5 py-24 mx-auto flex">
-    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Waiting for you</h2>
-       <form @submit.prevent="getAppointment">
-      <!-- <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p> -->
-      <div class="relative mb-4">
-         <label for="email" class="leading-7 text-lg font-bold  text-primary-3">Date</label>
-              <input required type="date" min="{{current}}" v-on:change="changeDate($event)" v-model="form.date" id="email"    name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-      </div>
-
-      <div class="relative mb-4">
-       <label for="name" class="leading-7 text-lg font-bold  text-primary-3">Time</label>
-            <select required v-on:change="changeItem($event)" v-model="form.selected"  name="name" class="w-full  bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-              <option>Time available</option>
-              <option v-for="times in time"  :key="times"  :value="times.CID" >{{times.begin}} to {{times.end}} to </option>
-            </select>
-      </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-lg font-bold  text-primary-3">Subject description</label>
-              <textarea required id="message" v-model="form.sujet" name="message" @input="getTextSubject($event.target.value)" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-           <input type="submit" value="Get Appointment" class="btn text-white px-3 py-2 rounded  w-full font-semibold bg-blue-500 hover:cursor-pointer hover:bg-blue-600 "> 
-</form>
+    <div class="absolute inset-0 bg-gray-300">
+      <iframe
+        style="filter: grayscale(1) contrast(1.2) opacity(0.4)"
+        marginheight="0"
+        marginwidth="0"
+        title="map"
+        scrolling="no"
+        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+        width="100%"
+        height="100%"
+        frameborder="0"
+      ></iframe>
     </div>
-  </div>
-</section>
+    <div class="container px-5 py-24 mx-auto flex">
+      <div
+        class="
+          lg:w-1/3
+          md:w-1/2
+          bg-white
+          rounded-lg
+          p-8
+          flex flex-col
+          md:ml-auto
+          w-full
+          mt-10
+          md:mt-0
+          relative
+          z-10
+          shadow-md
+        "
+      >
+        <h2
+          class="
+            text-slate-400
+            border-b-4 border-blue-500
+            pb-2
+            text-xl
+            uppercase
+            mb-1
+            font-semibold
+            title-font
+          "
+        >
+          Waiting for you
+        </h2>
+        <form @submit.prevent="getAppointment">
+          <!-- <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p> -->
+          <div class="relative mb-4">
+            <label
+              for="email"
+              class="leading-7 text-lg font-bold text-primary-3"
+              >Date</label
+            >
+            <input
+              required
+              type="date"
+              min="{{current}}"
+              v-on:change="changeDate($event)"
+              v-model="form.date"
+              id="email"
+              name="date"
+              class="
+                w-full
+                bg-gray-100 bg-opacity-50
+                rounded
+                border border-gray-300
+                focus:border-blue-500
+                focus:bg-white
+                focus:ring-2
+                focus:ring-blue-200
+                text-base
+                outline-none
+                text-gray-700
+                py-1
+                px-3
+                leading-8
+                transition-colors
+                duration-200
+                ease-in-out
+              "
+            />
+          </div>
 
+          <div class="relative mb-4">
+            <label for="name" class="leading-7 text-lg font-bold text-primary-3"
+              >Time</label
+            >
+            <select
+              required
+              v-on:change="changeItem($event)"
+              v-model="form.selected"
+              name="name"
+              class="
+                w-full
+                bg-opacity-50
+                rounded
+                border border-gray-300
+                focus:border-blue-500
+                focus:bg-white
+                focus:ring-2
+                focus:ring-blue-200
+                text-base
+                outline-none
+                text-gray-700
+                py-1
+                px-3
+                leading-8
+                transition-colors
+                duration-200
+                ease-in-out
+              "
+            >
+              <option>Time available</option>
+              <option v-for="times in time" :key="times" :value="times.CID">
+                {{ times.begin }} to {{ times.end }} to
+              </option>
+            </select>
+          </div>
+          <div class="relative mb-4">
+            <label
+              for="message"
+              class="leading-7 text-lg font-bold text-primary-3"
+              >Subject description</label
+            >
+            <textarea
+              required
+              id="message"
+              v-model="form.sujet"
+              name="message"
+              @input="getTextSubject($event.target.value)"
+              class="
+                w-full
+                bg-gray-100 bg-opacity-50
+                rounded
+                border border-gray-300
+                focus:border-blue-500
+                focus:bg-white
+                focus:ring-2
+                focus:ring-blue-200
+                h-32
+                text-base
+                outline-none
+                text-gray-700
+                py-1
+                px-3
+                resize-none
+                leading-6
+                transition-colors
+                duration-200
+                ease-in-out
+              "
+            ></textarea>
+          </div>
+          <input
+            type="submit"
+            value="Get Appointment"
+            class="
+              btn
+              text-white
+              px-3
+              py-2
+              rounded
+              w-full
+              font-semibold
+              bg-blue-500
+              hover:cursor-pointer hover:bg-blue-600
+            "
+          />
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
 
 
 
 <script>
+import swal from "sweetalert";
+
 export default {
   name: "bookAppointment",
-  data(){
+  data() {
     return {
       time: [],
-      selected:"",
-      date:"",
-      sujet:'',
-      current:'',
+      selected: "",
+      date: "",
+      sujet: "",
+      current: "",
 
-      form:{
-        date:this.date,
-        sujet:'',
-        id:localStorage.getItem('id'),
-        selected:this.selected
-      }
-
-    }
+      form: {
+        date: this.date,
+        sujet: "",
+        id: localStorage.getItem("id"),
+        selected: this.selected,
+      },
+    };
   },
-  components:{
-  },
-  methods:{
-    changeItem: function changeItem(event){
+  components: {},
+  methods: {
+    changeItem: function changeItem(event) {
       this.selected = event.target.value;
 
-      console.log(this.selected)
+      console.log(this.selected);
     },
 
-    getTextSubject(value){
-       this.sujet = value
+    getTextSubject(value) {
+      this.sujet = value;
     },
 
-    isExpired(){
-         let dateNow = new Date()
-        let  tokenTime=new Date(localStorage['exp'] *1000)
-        let  isExpired = tokenTime.getTime() - dateNow.getTime()
-        return isExpired
+    isExpired() {
+      let dateNow = new Date();
+      let tokenTime = new Date(localStorage["exp"] * 1000);
+      let isExpired = tokenTime.getTime() - dateNow.getTime();
+      return isExpired;
     },
 
-    changeDate: async function changeDate(event){
-        this.date = event.target.value;
+    changeDate: async function changeDate(event) {
+      this.date = event.target.value;
 
-        console.log(this.date);
+      console.log(this.date);
 
-      const data = await fetch('http://localhost/app/appointments/time',
-          {
-            method:'POST',
-            headers: {'content-type':'application/json'},
-            body:JSON.stringify({
-              selectedDate : this.date,})
+      const data = await fetch("http://localhost/app/appointments/time", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          selectedDate: this.date,
+        }),
+      });
 
-            })
-
-
-      if (data.status === 200){
-        let result = await  data.json();
+      if (data.status === 200) {
+        let result = await data.json();
 
         this.time = result;
 
-        console.log(this.time)
-        console.log(true)
-
+        console.log(this.time);
+        console.log(true);
       }
     },
 
-    async getAppointment(){
-   //  let token = localStorage.getItem("token");
-     /* let id = localStorage.getItem('id');
+    async getAppointment() {
+      //  let token = localStorage.getItem("token");
+      /* let id = localStorage.getItem('id');
       let date = this.date;
       let selected = this.selected;
       let sujet = this.sujet; */
-      const data = await fetch("http://localhost/app/appointments/takeAppointment",
-          {
-             method:"POST",
-            headers:{
-              "Content-Type":"application/json",
-            },
-            body:JSON.stringify(this.form),
-          })
+      const data = await fetch(
+        "http://localhost/app/appointments/takeAppointment",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.form),
+        }
+      );
 
-      if (data.status === 200){
-        console.log(this.form)
+      if (data.status === 200) {
+        console.log(this.form);
         console.log("appointment taken");
-      }else{
-        console.log("Failure")
-      }
-
-
-    } ,
-
-    async geTime(){
-      const data = await fetch('http://localhost/app/appointments/time',
-          {
-            method:'POST',
-            headers: {'content-type':'application/json'},
-          })
-
-      if (data.status === 200){
-          let result = await  data.json();
-
-          this.time = result;
-
-        console.log(this.time)
-        console.log(true)
-
+        swal({
+          title: "Good job!",
+          text: "You clicked the button!",
+          icon: "success",
+          button: "Aww yiss!",
+        });
+        this.$router.push("PatientPage");
+      } else {
+        console.log("Failure");
       }
     },
 
-     currentDate(){
-      const current = new Date()
-     const date = `${current.getFullYear()}-${current.getMonth()}-${current.getDay()}`
-      this.current = date
-      console.log(date)
-    }
+    async geTime() {
+      const data = await fetch("http://localhost/app/appointments/time", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+      });
+
+      if (data.status === 200) {
+        let result = await data.json();
+
+        this.time = result;
+
+        console.log(this.time);
+        console.log(true);
+      }
     },
+
+    currentDate() {
+      const current = new Date();
+      const date = `${current.getFullYear()}-${current.getMonth()}-${current.getDay()}`;
+      this.current = date;
+      console.log(date);
+    },
+  },
   beforeMount() {
-    this.geTime()
-    this.currentDate()
-  }
-}
-
+    this.geTime();
+    this.currentDate();
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
